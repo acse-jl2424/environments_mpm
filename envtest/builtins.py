5d0975146aa2fb3289a2bc07b934e4df012df316
acse-jyl130 <jiayu.li24@imperial.ac.uk>
2024-10-07 19:50:40 +0100

    add changes

--- envtest/builtins.py
+++ envtest/builtins.py
@@ -1,9 +1,10 @@
 import numpy as np
 from scipy.ndimage import gaussian_filter
 from scipy import misc
+import pandas as pd
 
 
-__all__ = ['rand_array', 'smooth_image', 'my_mat_solve']
+__all__ = ['rand_array', 'smooth_image', 'my_mat_solve','my_dataf']
 
 
 def rand_array(shape):
@@ -14,3 +15,6 @@ def smooth_image(a, sigma=1):
 
 def my_mat_solve(A, b):
     return A.inv()
+
+def my_dataf(M):
+    return pd.DataFrame(np.array(M))
---
+++ scripts/print_dataframe.py
@@ -0,0 +1,4 @@
+from envtest import my_dataf
+
+A=[1,2,3,4]
+print(my_dataf(A))
